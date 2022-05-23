ceb5b617871bcd36d93e15472c5a6014a73f35ac
EMEAAD\ccastillo <carlos.castillo2@soprasteria.com>
2022-05-23 16:19:02 +0200

    heroku intento de arreglo

--- src/main/java/com/app/GestionInmuebles/GestionInmueblesApplication.java
+++ src/main/java/com/app/GestionInmuebles/GestionInmueblesApplication.java
@@ -1,19 +1,13 @@
 package com.app.GestionInmuebles;
 
-import com.app.GestionInmuebles.Services.ServiceImpl.RolesServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GestionInmueblesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionInmueblesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionInmueblesApplication.class, args);
+    }
 }
